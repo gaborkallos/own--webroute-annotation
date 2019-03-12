5f38449293502c1a66972ee5bdede2a5075b879e
gaborkallos <gaborkallos@gmail.com>
2019-03-12 13:48:07 +0100

    refactored

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,57 +1,53 @@
 import java.io.IOException;
 import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
 
-import com.codecool.webrouteannotation.site.HttpMethod;
 import com.codecool.webrouteannotation.site.Routes;
 import com.codecool.webrouteannotation.site.WebRoute;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
-import static com.codecool.webrouteannotation.site.HttpMethod.GET;
-import static com.codecool.webrouteannotation.site.HttpMethod.POST;
 
 
 public class Test {
 
     public static void main(String[] args) throws Exception {
-
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-
-        for (Method m : Routes.class.getMethods()) {
-            if (m.isAnnotationPresent(WebRoute.class)) {
-            /*
-            Here comes your logic.
-            If the given path from the HttpExchange method is the SAME like the WebRoute annotation's path, you should INVOKE this method.
-            */
-                String route = m.getAnnotation(WebRoute.class).path();
-                System.out.println("POST");
-                String response = m.invoke(new Routes()).toString();
-                server.createContext(route, new MyHandler(response));
-                System.out.print(route);
-                System.out.println(" - " + response);
-
-            }
-        }
-
-
+        server.createContext("/", new MyHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
     }
 
     static class MyHandler implements HttpHandler {
-        private String response;
-
-        public MyHandler(String response) {
-            this.response = response;
-        }
 
         @Override
-        public void handle(HttpExchange t) throws IOException {
-            t.sendResponseHeaders(200, response.length());
-            OutputStream os = t.getResponseBody();
+        public void handle(HttpExchange httpExchange) throws IOException {
+            String response = null;
+            int statusCode = 200;
+            for (Method m : Routes.class.getMethods()) {
+                if (m.isAnnotationPresent(WebRoute.class)) {
+                    String path = m.getAnnotation(WebRoute.class).path();
+                    if (path.equals(httpExchange.getRequestURI().getPath())) {
+                        try {
+                            response = (String) m.invoke(new Routes());
+                        } catch (IllegalAccessException | InvocationTargetException e) {
+                            e.printStackTrace();
+                            statusCode = 500;
+                            response = "Internal server error";
+                        }
+                    }
+                }
+            }
+            if(response == null){
+                statusCode = 404;
+                response = "Page not found";
+            }
+
+            httpExchange.sendResponseHeaders(statusCode, response.length());
+            OutputStream os = httpExchange.getResponseBody();
             os.write(response.getBytes());
             os.close();
         }
--- src/main/java/com/codecool/webrouteannotation/site/WebRoute.java
+++ src/main/java/com/codecool/webrouteannotation/site/WebRoute.java
@@ -8,7 +8,6 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface WebRoute {
-//    String value() default "";
     HttpMethod method() default HttpMethod.GET;
     String path() default "";
 }
